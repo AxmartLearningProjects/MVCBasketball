b42786db44942b66547f4ff948842d917735992c
Axmart <notaxmart@gmail.com>
2020-10-23 20:02:36 +0200

    Insert Data Annotations on Raptors

--- MVCBasketball/Models/Raptors.cs
+++ MVCBasketball/Models/Raptors.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,17 @@ namespace MVCBasketball.Models
     {
 
         public int Id { get; set; }
+        
+        [Display(Name ="Player Number")]
         public int PlayerNumber { get; set; }
+        [Display(Name ="Player Name")]
         public string PlayerName { get; set; }
+        [Display(Name ="Position")]
         public string PlayerPosition { get; set; }
+        [Display(Name ="Height")]
         public string PlayerHeight { get; set; }
+        [Display(Name = "Salary")]
+        [Column(TypeName = "double(18,2)")]
         public double PlayerSalary { get; set; }
     }
 }
